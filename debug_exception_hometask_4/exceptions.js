98816394c3e3618fc73d60815817162d7abac79e
asliusa <andrii.sliusar@teadochealth.com>
2023-12-07 13:08:42 +0200

    add second task for exceptions

--- debug_exception_hometask_4/exceptions.js
+++ debug_exception_hometask_4/exceptions.js
@@ -14,3 +14,42 @@ function calcRectangleArea(width, height) {
     console.error(error);
   }
 }
+
+
+//2.1 Implement the getUser(id) function
+let users = 
+[
+  {'userId': 1, 'first_name': 'John'},
+  {'userId': 2, 'first_name': 'Todd'},
+  {'userId': 3, 'first_name': 'Threnton'}
+]
+
+
+function getUser(id) {
+try{
+ if (id<=0) {throw new Error('User id should be above 0')}
+  for(var i=0; i<users.length; i++) {
+   for(key in users[i]) {
+     if(users[i][key] == id) {
+       return users[i]
+     }
+    }
+  }
+ }
+ catch(error) {
+   console.error(error);
+ }
+}
+
+//2.2 Implement the getUsers(ids) function
+
+function getUsers(ids) {
+   let validUsersList = [];
+   for (i=0; i<=ids.length-1; i++){
+     if (getUser(ids[i])!==undefined) {validUsersList.push(getUser(ids[i]))};
+   }
+   return validUsersList;
+}
+
+// 3. 
+
